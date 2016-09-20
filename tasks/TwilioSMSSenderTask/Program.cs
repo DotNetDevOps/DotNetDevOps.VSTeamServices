66588744c824a7b507a741d5731fb14c87ce1229
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-09-20 19:21:43 +0200

    added a twilio txt sender

---
+++ tasks/TwilioSMSSenderTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+using SInnovations.VSTeamServices.TasksBuilder.Attributes;
+using SInnovations.VSTeamServices.TasksBuilder.ConsoleUtils;
+using Twilio;
+
+namespace TwilioSMSSenderTask
+{
+
+    [EntryPoint("Sending sms to $(ReceiverNumber)")]
+    public class ProgramOptions
+    {
+        [Option("AccountSid")]
+        public string AccountSid { get; set; }
+        [Option("AuthToken")]
+        public string AuthToken { get; set; }
+        [Option("Message")]
+        public string Message { get; set; }
+        [Option("ReceiverNumber")]
+        public string ReceiverNumber { get; set; }
+        [Option("AccountSid")]
+        public string SenderNumber { get; set; }
+    }
+    public class Program
+    {
+        private static readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private static readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+#if DEBUG
+            args = new[] { "--build" };
+#endif
+
+            var ops = ConsoleHelper.ParseAndHandleArguments<ProgramOptions>("Sending SMS", args);
+            var client = new TwilioRestClient(ops.AccountSid, ops.AuthToken);
+
+            client.SendMessage(
+                   ops.SenderNumber, // From number, must be an SMS-enabled Twilio number
+                   ops.ReceiverNumber,     // To number, if using Sandbox see note above
+                                           // message content
+                   ops.Message
+            );
+
+            Console.WriteLine(string.Format("Sent message to {0}", ops.ReceiverNumber));
+        }
+
+
+    }
+}
