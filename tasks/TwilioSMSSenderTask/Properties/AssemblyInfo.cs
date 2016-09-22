d8b34827ce1a3d833712efa2900b211514e92537
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-09-22 20:04:16 +0200

    New version of sms sender

--- tasks/TwilioSMSSenderTask/Properties/AssemblyInfo.cs
+++ tasks/TwilioSMSSenderTask/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Twilio SMS Sender")]
 [assembly: AssemblyDescription("Send sms with Twilio")]
-[assembly: AssemblyInformationalVersion("1.0.3")]  //Update to do new release
+[assembly: AssemblyInformationalVersion("1.0.4")]  //Update to do new release
 [assembly: AssemblyConfiguration("Utility")]
 [assembly: AssemblyCompany("S-Innovations v/Poul K. Sørensen")]
 [assembly: AssemblyProduct("TwilioSMSSenderTask")]
